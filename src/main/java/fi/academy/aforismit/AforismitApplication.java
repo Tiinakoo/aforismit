9e1cf5ab6fd0d527e716dd72beedd81c1104c844
Tiinakooo <tiina.hrd@gmail.com>
2018-03-01 10:42:45 +0200

    pohja lisätty

---
+++ src/main/java/fi/academy/aforismit/AforismitApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.aforismit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AforismitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AforismitApplication.class, args);
+	}
+}
